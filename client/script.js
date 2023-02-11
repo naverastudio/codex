3e0a06fbc242c26930c7d5d72b635aaf9ed62c13
naverastudio <support@getempowerpro.com>
2023-02-11 15:00:01 -0500

    Update client and server scripts

--- client/script.js
+++ client/script.js
@@ -10,8 +10,10 @@ function loader(element) {
     element.textContent = '';
 
     loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
         element.textContent += '.';
 
+        // If the loading indicator has reached three dots, reset it
         if (element.textContent === '....') {
             element.textContent = '';
         }
@@ -31,6 +33,9 @@ function typeText(element, text) {
     }, 20);
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -63,25 +68,21 @@ const handleSubmit = async (e) => {
     // user's chatstripe
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
 
+    // to clear the textarea input
     form.reset();
 
     // bot's chatstripe
     const uniqueId = generateUniqueId();
     chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
 
-    // Smooth scrolling between the chat and response
-
-    const chatWrapper = document.querySelector('.wrapper:first-child');
-    chatWrapper.scrollIntoView({ behavior: 'smooth' });
-
     // to focus scroll to the bottom
-    window.scrollTo(0, chatContainer.scrollHeight);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
 
+    // specific message div
     const messageDiv = document.getElementById(uniqueId);
 
+    // messageDiv.innerHTML = "..."
     loader(messageDiv);
-
-    // fetch data from server -> bot's response
 
     const response = await fetch('https://codex-0nys.onrender.com', {
         method: 'POST',
@@ -92,26 +93,26 @@ const handleSubmit = async (e) => {
             prompt: data.get('prompt'),
         }),
     });
+
     clearInterval(loadInterval);
-    messageDiv.innerHTML = '';
+    messageDiv.innerHTML = ' ';
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim();
+        const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
 
         typeText(messageDiv, parsedData);
     } else {
         const err = await response.text();
 
         messageDiv.innerHTML = 'Something went wrong';
-
         alert(err);
     }
 };
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
+    if (e.keyCode === 13) {
         handleSubmit(e);
     }
 });
